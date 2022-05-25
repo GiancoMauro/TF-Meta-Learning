114f98d28bf371384ec36036c75b318463da6685
GiancoMauro <98472216+GiancoMauro@users.noreply.github.com>
2022-05-25 14:07:46 +0200

    minor bug solve

--- networks/weighting_modules.py
+++ networks/weighting_modules.py
@@ -1,45 +1,50 @@
 from abc import ABC
-
 import tensorflow as tf
 
 
 class Embedding_Module(tf.keras.Model, ABC):
-    """docstring for ClassName"""
+    """The Embedding Module takes as distinct input the support and query
+    examples and generate an embedding for the following comparison phase.
+    The training of this module is only performed on the support data for
+    the pure few shot learning."""
 
     def __init__(self, feature_dimension):
         super(Embedding_Module, self).__init__()
         self.layer1 = tf.keras.Sequential([
-            tf.keras.layers.Conv2D(filters=64, kernel_size=5, padding="valid"),  # kernel_initializer=initializer),
+            tf.keras.layers.Conv2D(filters=64, kernel_size=7, padding="valid"),  # kernel_initializer=initializer),
             tf.keras.layers.BatchNormalization(momentum=1),
             tf.keras.layers.ReLU(),
             tf.keras.layers.MaxPool2D(2)])
         self.layer2 = tf.keras.Sequential([
-            tf.keras.layers.Conv2D(filters=64, kernel_size=(5, 4), padding="valid"),  # kernel_initializer=initializer),
+            tf.keras.layers.Conv2D(filters=64, kernel_size=(7, 6), padding="valid"),  # kernel_initializer=initializer),
             tf.keras.layers.BatchNormalization(momentum=1),
             tf.keras.layers.ReLU(),
             tf.keras.layers.MaxPool2D(2)])
         self.layer3 = tf.keras.Sequential([
-            tf.keras.layers.Conv2D(filters=64, kernel_size=(4, 5), padding="valid"),
+            tf.keras.layers.Conv2D(filters=64, kernel_size=(6, 7), padding="valid"),
             tf.keras.layers.ZeroPadding2D(1),
             tf.keras.layers.BatchNormalization(momentum=1),
             tf.keras.layers.ReLU()])
         self.layer4 = tf.keras.Sequential([
             tf.keras.layers.Conv2D(
-                filters=feature_dimension, kernel_size=(5, 4), padding="valid"),
+                filters=feature_dimension, kernel_size=(7), padding="valid"),
             tf.keras.layers.BatchNormalization(momentum=1),
             tf.keras.layers.ReLU(name="embedding")])
 
     def call(self, inp):
-        # input shape: (?, 64, 32, 3)
+        # input shape: (?, 105, 105, 1)
         out = self.layer1(inp)
         out = self.layer2(out)
         out = self.layer3(out)
         out = self.layer4(out)
-        return out  # (?, 1, 1, 64)
+        print("OUT EMBB:", out.shape)
+        return out  # (?, 18, 18, 32)
 
 
 class Comparison_Module(tf.keras.Model, ABC):
-    """docstring for WeighingNetwork"""
+    """The Comparison module take as input the channel-wise concatenation of support and query samples.
+    It enables the extraction of mixed features. For N available support per class,
+    the generated comparison vectors are averaged over N"""
 
     def __init__(self, feature_dimension, classes):
         super(Comparison_Module, self).__init__()
@@ -61,19 +66,20 @@ class Comparison_Module(tf.keras.Model, ABC):
         ])
 
     def call(self, inp, num_train_shots):
-        # print("Concatenated Examples: ", inp.shape)
+        print("Concatenated Examples: ", inp.shape)
         out = self.layer1(inp)
         out = self.layer2(out)
-        # print("B SHAPE: ", out.shape)
+        print("B SHAPE: ", out.shape)
         out = tf.reshape(out, [-1, self.classes, num_train_shots, self.feature_dimension])
         out = tf.math.reduce_mean(out, axis=2, keepdims=False)
         out = tf.reshape(out, [-1, self.classes * self.feature_dimension])
-        # print("C SHAPE: ", out.shape)
+        print("C SHAPE: ", out.shape)
         return out
 
 
 class Weighting_Module(tf.keras.Model, ABC):
-    """docstring for WeighingNetwork"""
+    """The Weighting Module takes as input the comparison vectors and generates
+    a distribution of probabilities over the available classes"""
 
     def __init__(self, weighting_dim, classes):
         super(Weighting_Module, self).__init__()
@@ -81,16 +87,15 @@ class Weighting_Module(tf.keras.Model, ABC):
         self.fc2 = tf.keras.layers.Dense(classes, activation="softmax")  # kernel_initializer=initializer)
 
     def call(self, inp):
-        # print("Concatenated Examples: ", inp.shape)
+        print("Weighting Examples: ", inp.shape)
         out = self.fc1(inp)
-        # print("D SHAPE: ", out.shape)
         out = self.fc2(out)
-        # print("E SHAPE: ", out.shape)
+        print("E SHAPE: ", out.shape)
         return out
 
 
 class Full_Pipeline(tf.keras.Model, ABC):
-    """docstring for WeighingNetwork"""
+    """The Full Pipeline handles the information flow for Embedding, Comparison and Weighting Modules"""
 
     def __init__(self, classes, feature_dimension, weighting_dim):
         super(Full_Pipeline, self).__init__()
@@ -109,7 +114,7 @@ class Full_Pipeline(tf.keras.Model, ABC):
 
         support_features = self.embedding_model(support_samples)
 
-        # print(support_features.shape)
+        print(support_features.shape)
         if multi_query:
             # if more than 1 query, the support have to be replicated N times respect to the number of query shots
             support_features_ext = tf.repeat(tf.expand_dims(support_features, 0), num_shots_qr_ts * batch_s, axis=0)
@@ -121,13 +126,13 @@ class Full_Pipeline(tf.keras.Model, ABC):
         # extend the query for the number of classes (1-support each class)
         query_features_ext = tf.repeat(tf.expand_dims(query_features, 0), num_shots_tr * self.classes, axis=0)
         query_features_ext = tf.experimental.numpy.moveaxis(query_features_ext, 0, 1)
-        # print("extended query: ", query_features_ext.shape)
+        print("extended query: ", query_features_ext.shape)
         comparison_features = self.concat([support_features_ext, query_features_ext])
-        # print(comparison_features.shape)
-        comparison_features = tf.reshape(comparison_features, [-1, self.feature_dimension * 2, 18, 18])
-        # print(comparison_features.shape)
+        comparison_features = tf.reshape(comparison_features, [-1, self.feature_dimension * 2, 12, 12])
+        print(comparison_features.shape)
         weighting_features = self.comparison_model(comparison_features, num_shots_tr)
 
         out = self.weighting_model(weighting_features)
-
+        print("FINAL: ", out.shape)
+        input()
         return out
